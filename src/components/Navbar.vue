b25d2a36b9c2941a3ff2d2b1fe4ea7cc69e9acbc
mccrush <mccrush2027@gmail.com>
2020-07-02 09:52:27 +0700

    Config navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/img/logo.png"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt
+          loading="lazy"
+        />
+        MWP
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- <div>
     <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
-  </div>
+  </div>-->
 </template>
