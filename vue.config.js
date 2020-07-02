26f2a955d8e4c27fe9a6141bfaeaf045ca59d20b
mccrush <mccrush2027@gmail.com>
2020-07-02 09:29:37 +0700

    Config start project

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+module.exports = {
+  // options...
+  productionSourceMap: false,
+  pwa: {
+    name: 'MWP',
+    themeColor: '#007ACC',
+    manifestOptions: {
+      background_color: '#007ACC'
+    }
+  }
+}
