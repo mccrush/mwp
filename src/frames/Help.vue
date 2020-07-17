a1602aa3bb6e2c41dcde0f64c354ecbf5c77d5d0
mccrush <mccrush2027@gmail.com>
2020-07-17 17:40:41 +0700

    Add Edit page

--- src/frames/Help.vue
+++ src/frames/Help.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <div class="border-bottom p-2">
-        <button @click="mode = 'read'" class="btn btn-light btn-sm">Read</button>
-        <button @click="mode = 'edit'" class="btn btn-light btn-sm ml-3">Edit</button>
+      <div class="border-bottom p-2 pl-0">
+        <button
+          @click="mode = 'read'"
+          class="btn btn-light btn-sm"
+          :class="mode === 'read' ? ' active' : ''"
+        >Read</button>
+        <button
+          @click="mode = 'edit'"
+          class="btn btn-light btn-sm ml-2"
+          :class="mode === 'edit' ? ' active' : ''"
+        >Edit</button>
       </div>
       <Login :mode="mode" />
     </div>
