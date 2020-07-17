88c5cf7cf09a7b1252772d9c7bdf59b97418a77b
mccrush <mccrush2027@gmail.com>
2020-07-17 18:53:41 +0700

    Restruct component

--- src/frames/Help.vue
+++ src/frames/Help.vue
@@ -1,33 +1,52 @@
 <template>
-  <div class="row">
-    <div class="col-12">
-      <div class="border-bottom p-2 pl-0">
-        <button
-          @click="mode = 'read'"
-          class="btn btn-light btn-sm"
-          :class="mode === 'read' ? ' active' : ''"
-        >Read</button>
-        <button
-          @click="mode = 'edit'"
-          class="btn btn-light btn-sm ml-2"
-          :class="mode === 'edit' ? ' active' : ''"
-        >Edit</button>
-      </div>
-      <Login :mode="mode" />
+  <div class="row p-1 pt-2">
+    <div v-for="(login, index) in logins" :key="'lg'+index" class="col-6 p-2">
+      <table class="table table-bordered border-left border-right table-sm m-0">
+        <tr>
+          <td class="w-50 border-right">
+            <div class="input-group input-group-sm p-0">
+              <input type="text" class="form-control form-control-sm border-0 rounded-0" />
+            </div>
+          </td>
+          <td class="p-0">
+            <div class="input-group input-group-sm p-0">
+              <input type="text" class="form-control form-control-sm border-0 rounded-0" />
+              <button class="btn btn-light rounded-0" type="button" title="Open">o</button>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td class="p-0 w-50 border-right">
+            <div class="input-group input-group-sm p-0">
+              <input type="text" class="form-control form-control-sm border-0 rounded-0" />
+              <button class="btn btn-light rounded-0" type="button" title="Copy">c</button>
+            </div>
+          </td>
+          <td class="p-0">
+            <div class="input-group input-group-sm p-0">
+              <input type="text" class="form-control form-control-sm border-0 rounded-0" />
+              <button class="btn btn-light rounded-0" type="button" title="Copy">c</button>
+            </div>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import Login from '@/components/login/Login'
-
 export default {
-  components: {
-    Login
-  },
   data() {
     return {
-      mode: 'read'
+      logins: [
+        { name: '', host: '', login: '', pass: '' },
+        { name: '', host: '', login: '', pass: '' }
+      ]
+    }
+  },
+  computed: {
+    logins() {
+      return false
     }
   }
 }
