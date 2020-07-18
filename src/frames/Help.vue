b291c0fc591868ef1be0cfba82a4fe15a0f62c3b
mccrush <mccrush2027@gmail.com>
2020-07-18 23:49:01 +0700

    Change but text

--- src/frames/Help.vue
+++ src/frames/Help.vue
@@ -2,7 +2,7 @@
   <div class="row p-1 pt-2">
     <LoginForm v-for="(login, index) in logins" :key="'lg'+index" :logins="logins" :login="login" />
     <div class="col-6 col-lg-4 p-2">
-      <button @click="create" class="btn btn-sm btn-light text-muted">Добавить</button>
+      <button @click="create" class="btn btn-sm btn-light text-muted">Add</button>
     </div>
   </div>
 </template>
