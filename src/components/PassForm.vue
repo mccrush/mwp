6d497cd87f757ad21e82f85555216ec483e1aa1e
mccrush <mccrush2027@gmail.com>
2021-05-11 15:36:48 +0700

    Create Pass component

---
+++ src/components/PassForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col-12 col-md-6 border rounded p-2">
+    <input
+      type="text"
+      class="form-control form-control-sm"
+      placeholder="Title"
+    />
+    <!-- -->
+    <div class="input-group mt-1">
+      <input
+        type="text"
+        class="form-control form-control-sm"
+        placeholder="Login"
+      />
+      <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
+        <img
+          src="img/work_icons/files.svg"
+          width="16"
+          height="16"
+          class="opacity-06 mt-1 mb-1"
+        />
+      </button>
+    </div>
+    <!-- -->
+    <div class="input-group mt-1">
+      <input
+        type="password"
+        autocomplete="new-password"
+        class="form-control form-control-sm"
+        placeholder="Password"
+      />
+      <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
+        <img
+          src="img/work_icons/eye-slash.svg"
+          width="16"
+          height="16"
+          class="opacity-06 mt-1 mb-1"
+        />
+      </button>
+      <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
+        <img
+          src="img/work_icons/files.svg"
+          width="16"
+          height="16"
+          class="opacity-06 mt-1 mb-1"
+        />
+      </button>
+    </div>
+    <!-- -->
+    <textarea
+      rows="2"
+      class="form-control mt-1"
+      style="white-space: pre-line"
+      placeholder="Comment"
+    ></textarea>
+    <div class="d-flex justify-content-between">
+      <button class="btn btn-sm btn-outline-danger mt-1">Remove form</button>
+      <button class="btn btn-sm btn-light border mt-1">Save form</button>
+    </div>
+  </div>
+</template>
--- src/frames/Pass.vue
+++ src/frames/Pass.vue
@@ -1,81 +1,22 @@
 <template>
   <div class="col-12 col-sm-8 col-md-9 col-lg-10 col-xl-10 col-xxl-10">
     <div class="row p-2">
-      <div class="col-12 col-md-6 border rounded p-2">
-        <input
-          type="text"
-          class="form-control form-control-sm"
-          placeholder="Title"
-        />
-        <!-- -->
-        <div class="input-group mt-1">
-          <input
-            type="text"
-            class="form-control form-control-sm"
-            placeholder="Login"
-          />
-          <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
-            <img
-              src="img/work_icons/files.svg"
-              width="16"
-              height="16"
-              class="opacity-06 mt-1 mb-1"
-            />
-          </button>
-        </div>
-        <!-- -->
-        <div class="input-group mt-1">
-          <input
-            type="password"
-            autocomplete="new-password"
-            class="form-control form-control-sm"
-            placeholder="Password"
-          />
-          <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
-            <img
-              src="img/work_icons/eye-slash.svg"
-              width="16"
-              height="16"
-              class="opacity-06 mt-1 mb-1"
-            />
-          </button>
-          <button class="btn btn-sm btn-light border p-0 ps-3 pe-3">
-            <img
-              src="img/work_icons/files.svg"
-              width="16"
-              height="16"
-              class="opacity-06 mt-1 mb-1"
-            />
-          </button>
-        </div>
-        <!-- -->
-        <textarea
-          rows="2"
-          class="form-control mt-1"
-          style="white-space: pre-line"
-          placeholder="Comment"
-        ></textarea>
-        <div class="d-flex justify-content-between">
-          <button class="btn btn-sm btn-outline-danger mt-1">
-            Remove form
-          </button>
-          <button class="btn btn-sm btn-light border mt-1">Save form</button>
-        </div>
-      </div>
+      <PassForm />
     </div>
   </div>
 </template>
 
 <script>
+import PassForm from '@/components/PassForm'
+
 export default {
-  data() {
-    return {
-      title: '',
-      url: ''
-    }
+  components: {
+    PassForm
   },
   methods: {
-    update() {}
+    addForm() {},
+    updateForm() {},
+    removeForm() {}
   }
 }
 </script>
